3edf10195329682b7b749c79097798565f0a2d52
dfkeenan <dfkeenan@gmail.com>
2023-04-08 13:01:10 +1000

    Do some suggested code fixes.

--- Examples/Vulkan/GettingStarted/Program.cs
+++ Examples/Vulkan/GettingStarted/Program.cs
@@ -423,11 +423,9 @@ unsafe class HelloTriangleApplication
             {
                 SType = StructureType.DeviceQueueCreateInfo,
                 QueueFamilyIndex = uniqueQueueFamilies[i],
-                QueueCount = 1
+                QueueCount = 1,
+                PQueuePriorities = &queuePriority
             };
-
-
-            queueCreateInfos[i].PQueuePriorities = &queuePriority;
         }
 
         PhysicalDeviceFeatures deviceFeatures = new()
@@ -1108,8 +1106,7 @@ unsafe class HelloTriangleApplication
 
     private void CreateTextureSampler()
     {
-        PhysicalDeviceProperties properties;
-        vk.GetPhysicalDeviceProperties(physicalDevice, out properties);
+        vk.GetPhysicalDeviceProperties(physicalDevice, out PhysicalDeviceProperties properties);
 
         SamplerCreateInfo samplerInfo = new()
         {
@@ -1166,9 +1163,8 @@ unsafe class HelloTriangleApplication
 
         };
 
-        ImageView imageView = default;
 
-        if (vk.CreateImageView(device, createInfo, null, out imageView) != Result.Success)
+        if (vk.CreateImageView(device, createInfo, null, out ImageView imageView) != Result.Success)
         {
             throw new Exception("failed to create image views!");
         }
@@ -1206,8 +1202,7 @@ unsafe class HelloTriangleApplication
             }
         }
 
-        MemoryRequirements memRequirements;
-        vk.GetImageMemoryRequirements(device, image, out memRequirements);
+        vk.GetImageMemoryRequirements(device, image, out MemoryRequirements memRequirements);
 
         MemoryAllocateInfo allocInfo = new()
         {
@@ -1338,7 +1333,7 @@ unsafe class HelloTriangleApplication
                         var position = mesh->MVertices[index];
                         var texture = mesh->MTextureCoords[0][(int)index];
 
-                        Vertex vertex = new Vertex
+                        Vertex vertex = new ()
                         {
                             pos = new Vector3D<float>(position.X, position.Y, position.Z),
                             color = new Vector3D<float>(1, 1, 1),
@@ -1584,8 +1579,7 @@ unsafe class HelloTriangleApplication
             CommandBufferCount = 1,
         };
 
-        CommandBuffer commandBuffer = default;
-        vk.AllocateCommandBuffers(device, allocateInfo, out commandBuffer);
+        vk.AllocateCommandBuffers(device, allocateInfo, out CommandBuffer commandBuffer);
 
         CommandBufferBeginInfo beginInfo = new()
         {
@@ -1631,8 +1625,7 @@ unsafe class HelloTriangleApplication
 
     private uint FindMemoryType(uint typeFilter, MemoryPropertyFlags properties)
     {
-        PhysicalDeviceMemoryProperties memProperties;
-        vk.GetPhysicalDeviceMemoryProperties(physicalDevice, out memProperties);
+        vk.GetPhysicalDeviceMemoryProperties(physicalDevice, out PhysicalDeviceMemoryProperties memProperties);
 
         for (int i = 0; i < memProperties.MemoryTypeCount; i++)
         {
@@ -2005,8 +1998,7 @@ unsafe class HelloTriangleApplication
             swapChainAdequate = swapChainSupport.Formats.Any() && swapChainSupport.PresentModes.Any();
         }
 
-        PhysicalDeviceFeatures supportedFeatures;
-        vk.GetPhysicalDeviceFeatures(device, out supportedFeatures);
+        vk.GetPhysicalDeviceFeatures(device, out PhysicalDeviceFeatures supportedFeatures);
 
         return indices.IsComplete() && extensionsSupported && swapChainAdequate && supportedFeatures.SamplerAnisotropy;
     }
